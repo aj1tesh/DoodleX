3f568861941a501a5f300bd10c0c5aa8956634d3
Ajitesh <singh.ajitesh1449@gmail.com>
2026-05-04 17:08:16 +0530

    #6.1 Board List refinements

--- app/(dashboard)/_components/sidebar/board-list.tsx
+++ app/(dashboard)/_components/sidebar/board-list.tsx
@@ -26,8 +26,16 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
 
     if(data===undefined) {
         return (
-            <div className="flex items-center justify-center h-full">
-                <Loader2 className="w-4 h-4 animate-spin" />
+            <div>
+                <h2 className="text-2xl font-bold">
+                    {query.favorite ? "Favorite Boards" : "All Boards"}
+                </h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+                    <NewBoardButton orgId={orgId} disabled/>
+                    {Array.from({ length: 10 }).map((_, index) => (
+                        <BoardCard.Skeleton key={index} />
+                    ))}
+                </div>
             </div>
         )
     }
